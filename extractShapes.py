e5ea169635581019d2bb6ffc9f87f26200855d2d
HsinYeh <thankyouyou06@gmail.com>
2023-05-17 03:10:50 +0800

    update

--- extractShapes.py
+++ extractShapes.py
@@ -28,53 +28,53 @@ def main():
     binxcenters = []
 
     # import ROOT stuff
-    # from ROOT import TFile, TH1F, TH1D
-    # # open input file
-    # input_file = TFile(args.input_file)
+    from ROOT import TFile, TH1F, TH1D
+    # open input file
+    input_file = TFile(args.input_file)
 
-    # directory = input_file
-    # if args.dir != '':
-    #     directory = input_file.Get(args.dir)
+    directory = input_file
+    if args.dir != '':
+        directory = input_file.Get(args.dir)
 
-    # # get the number of histograms
-    # nEntries = directory.GetListOfKeys().GetEntries()
+    # get the number of histograms
+    nEntries = directory.GetListOfKeys().GetEntries()
 
 
-    # # loop over histograms in the input ROOT file
-    # for h in range(0, nEntries):
-    #     hName = directory.GetListOfKeys()[h].GetName()
-    #     print(hName)
-    #     mass = int(hName.split('_')[-2].replace('M',''))
+    # loop over histograms in the input ROOT file
+    for h in range(0, nEntries):
+        hName = directory.GetListOfKeys()[h].GetName()
+        print(hName)
+        mass = int(hName.split('_')[-2].replace('M',''))
 
-    #     if args.debug: print "Extracting shapes for m =", mass, "GeV..."
+        if args.debug: print "Extracting shapes for m =", mass, "GeV..."
 
-    #     histo = directory.Get(hName)
+        histo = directory.Get(hName)
 
-    #     #print(histo)
+        #print(histo)
 
-    #     bincontents = []
+        bincontents = []
 
-    #     for i in range(1,histo.GetNbinsX()+1):
-    #         bincontents.append(histo.GetBinContent(i))
-    #         if len(binxcenters) < histo.GetNbinsX():
-    #             binxcenters.append(histo.GetBinCenter(i))
+        for i in range(1,histo.GetNbinsX()+1):
+            bincontents.append(histo.GetBinContent(i))
+            if len(binxcenters) < histo.GetNbinsX():
+                binxcenters.append(histo.GetBinCenter(i))
 
-    #     normbincontents = np.array(bincontents)
-    #     normbincontents = normbincontents/np.sum(normbincontents)
+        normbincontents = np.array(bincontents)
+        normbincontents = normbincontents/np.sum(normbincontents)
 
-    #     shapes[mass] = normbincontents.tolist()
+        shapes[mass] = normbincontents.tolist()
 
-    # if args.debug: print ""
-    # if args.debug: print "Extracted shapes:"
-    # if args.debug: print ""
-    # print "shapes = {\n"
-    # for key, value in sorted(shapes.items()):
-    #     print("  {} : {},".format(key, value))
-    #     print ""
-    # print "}"
-    # print ""
-    # print "binxcenters =", binxcenters
-    # print ""
+    if args.debug: print ""
+    if args.debug: print "Extracted shapes:"
+    if args.debug: print ""
+    print "shapes = {\n"
+    for key, value in sorted(shapes.items()):
+        print("  {} : {},".format(key, value))
+        print ""
+    print "}"
+    print ""
+    print "binxcenters =", binxcenters
+    print ""
 
 
 if __name__ == '__main__':
